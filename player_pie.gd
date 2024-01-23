a33e653fc42ea395e940deefe68dbc6c959c998c
Natasha Draper <natasha@draper.net.za>
2024-01-23 18:29:12 +0200

    feat: fix throwing of pies

--- player_pie.gd
+++ player_pie.gd
@@ -3,20 +3,23 @@ extends Area2D
 const SPEED = 1000
 const RANGE = 1200
 
+@export var velocity = 1000
+
 var travelled_distance = 0
+var initial_position: Vector2
 
 func _physics_process(delta):
-	var direction = Vector2.UP.rotated(rotation)
-	position += direction * SPEED * delta
-	travelled_distance += SPEED * delta
+	transform.origin += velocity * delta
 	
-	if travelled_distance > RANGE:
+	if (transform.origin - initial_position).length() >= RANGE:
 		queue_free()
 		
-
+func _init():
+	initial_position = transform.origin
 
 func _on_body_entered(body):
-	queue_free()
-	
 	if body.has_method("take_hit"):
 		body.take_hit()
+			
+	queue_free()
+	
