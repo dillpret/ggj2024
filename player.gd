c73959462140124f7386b6c3a9a8c27ccb10e2f5
msklein <klein_m@hotmail.com>
2024-01-23 18:34:48 +0200

    changed spwan order for pies, fixed rapid fire of pies

--- player.gd
+++ player.gd
@@ -10,11 +10,12 @@ func _physics_process(delta):
 func throw():
 	const PLAYER_PIE = preload("res://player_pie.tscn")
 	var new_pie = PLAYER_PIE.instantiate()
+	%ThrowPoint.add_child(new_pie)
 	new_pie.global_position = %ThrowPoint.global_position
 	new_pie.global_scale = Vector2(1,1)
 	#new_pie.global_rotation = %ThrowPoint
-	%ThrowPoint.add_child(new_pie)
+	
 
 func _process(delta):
-	if Input.is_action_pressed("throw_pie"):
+	if Input.is_action_just_pressed("throw_pie"):
 		throw()
