fc26b478c2285a6a17c27cc92b01b1b2d6aacf32
msklein <klein_m@hotmail.com>
2024-01-26 23:29:03 +0200

    enemy spawner and enemies colliding with each other

---
+++ entities/enemies/enemy_spawner.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+@export var enemy: Resource
+
+@onready var radius = $CollisionShape2D.get_shape().radius
+	
+func spawn_enemy():
+	var new_enemy = enemy.instantiate()
+	var degrees = randf_range(0, 360)
+	var position_on_circle = Vector2(radius * cos(deg_to_rad(degrees)), radius * sin(deg_to_rad(degrees)))
+	
+	add_child(new_enemy)
+	
+	new_enemy.global_position = position_on_circle
+
+
+func _on_timer_timeout():
+	spawn_enemy()
