b52b278406a3b7e7597daf8d9583f115aa0f50d1
Dillon Pretorius <pretorius.dillon@gmail.com>
2024-01-27 13:28:50 +0200

    added ring collision and tweaked enemy spawning to fit ellipse

--- entities/enemies/enemy_spawner.gd
+++ entities/enemies/enemy_spawner.gd
@@ -1,18 +1,16 @@
-extends Area2D
+extends Node2D
 
 @export var enemy: Resource
-
-@onready var radius = $CollisionShape2D.get_shape().radius
 	
 func spawn_enemy():
 	var new_enemy = enemy.instantiate()
-	var degrees = randf_range(0, 360)
-	var position_on_circle = Vector2(radius * cos(deg_to_rad(degrees)), radius * sin(deg_to_rad(degrees)))
+	
+	%PathFollow2D.progress_ratio = randf()
 	
 	add_sibling(new_enemy)
 	
 	new_enemy.owner = get_parent()
-	new_enemy.global_position = position_on_circle
+	new_enemy.global_position = %PathFollow2D.global_position
 
 func _on_timer_timeout():
 	spawn_enemy()
