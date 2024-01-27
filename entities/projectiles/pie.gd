7d28d0e0f9ec7c7128f07b74600053875cb0ef48
msklein <klein_m@hotmail.com>
2024-01-27 23:03:20 +0200

    added camera shake on player hit

--- entities/projectiles/pie.gd
+++ entities/projectiles/pie.gd
@@ -2,8 +2,14 @@ extends Area2D
 
 const RANGE = 1200
 
+@onready var camera: Camera2D = get_node("/root/Game/Player/Camera2D")
+
 @export var velocity: Vector2
 @export var damage: float = 10
+@export var max_camera_shake_strength: float = 30
+@export var camera_shake_fade: float = 5
+
+var camera_shake_strength: float = 0
 
 var initial_position: Vector2
 
@@ -12,6 +18,11 @@ func _physics_process(delta):
 
 	if (global_position - initial_position).length() >= RANGE:
 		queue_free()
+		
+	if camera_shake_strength > 0:
+			camera_shake_strength = lerpf(camera_shake_strength, 0, camera_shake_fade * delta)
+			
+			camera.offset = random_offset()
 
 func _init():
 	initial_position = transform.origin
@@ -19,6 +30,16 @@ func _init():
 func _on_body_entered(body):
 	if body.has_method("take_hit"):
 		body.take_hit(damage)
+		
+		apply_camera_shake()
+		
+		
 
 	queue_free()
 
+func apply_camera_shake():
+	camera_shake_strength = max_camera_shake_strength
+	
+func random_offset() -> Vector2:
+	var random_shake_strength = randf_range(-camera_shake_strength, camera_shake_strength)
+	return Vector2(random_shake_strength, random_shake_strength)
